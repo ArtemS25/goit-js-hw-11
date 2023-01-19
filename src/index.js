36500c92542f5aba80e25f26666e5f50b01af5bb
ArtemS25 <110459689+ArtemS25@users.noreply.github.com>
2023-01-19 13:45:51 +0200

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,57 @@
+import { fetchCountries } from './js/fetchCountries';
+import './css/styles.css';
+import debounce from 'lodash.debounce';
+import Notiflix from 'notiflix';
+export { zeroingHTML };
+const DEBOUNCE_DELAY = 300;
+
+const countryList = document.querySelector('.country-list');
+countryList.style.listStyle = 'none';
+const countryInfo = document.querySelector('.country-info');
+countryInfo.style.listStyle = 'none';
+const input = document.querySelector('#search-box');
+
+function zeroingHTML() {
+  countryList.innerHTML = ' ';
+  countryInfo.innerHTML = ' ';
+}
+
+input.addEventListener('input', debounce(fetchCountries, DEBOUNCE_DELAY));
+
+export default function renderList(data) {
+  if (data.length > 10) {
+    Notiflix.Notify.info(
+      'Too many matches found. Please enter a more specific name.'
+    );
+  }
+  if (data.length > 2 && data.length <= 10) {
+    zeroingHTML();
+    const markupCoutryList = data
+      .map(
+        ({ flags, name }) =>
+          `<li class = "list__item"><img src="${flags.svg}"alt ="flags ${name}" width = 20> ${name}</li>`
+      )
+      .join('');
+    countryList.insertAdjacentHTML('afterbegin', markupCoutryList);
+  }
+  if (data.length == 1) {
+    zeroingHTML();
+    const markupCoutryList = data
+      .map(
+        ({ flags, name }) =>
+          `<li class = "list__item"><img src="${flags.svg}"alt ="flags ${name}" width = 20> ${name}</li>`
+      )
+      .join('');
+    countryList.insertAdjacentHTML('afterbegin', markupCoutryList);
+    const markupCoutryInfo = data.map(
+      ({
+        capital,
+        population,
+        languages,
+      }) => `<li class= "list__item">Capital:${capital}</li>
+            <li class= "list__item">Population:${population}</li>
+            <li class= "list__item">Languages:${languages[0].name}</li>`
+    );
+    countryInfo.insertAdjacentHTML('afterbegin', markupCoutryInfo);
+  }
+}
---
+++ src/js/fetchCountries.js
@@ -0,0 +1,28 @@
+export { fetchCountries };
+import Notiflix from 'notiflix';
+import renderList from '../index';
+import { zeroingHTML } from '../index';
+
+const input = document.querySelector('#search-box');
+let name = null;
+
+const URL = 'https://restcountries.com/v2/name/';
+
+function fetchCountries(name) {
+  name = input.value.trim();
+
+  if (name.length === 0) {
+    zeroingHTML();
+    return;
+  }
+
+  fetch(`${URL}${name}?fields=name,capital,population,flags,languages`)
+    .then(r => {
+      if (!r.ok) {
+        Notiflix.Notify.failure('Oops, there is no country with that name');
+        zeroingHTML();
+      }
+      return r.json();
+    })
+    .then(renderList);
+}
